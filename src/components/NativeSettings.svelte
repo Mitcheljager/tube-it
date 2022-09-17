4af8b11b5b40efa6c6f7309461f3985210119b33
Mitchel <mitchel_jager@hotmail.com>
2022-09-17 23:43:21 +0200

    Hide status bar on mount

--- src/components/NativeSettings.svelte
+++ src/components/NativeSettings.svelte
@@ -1,5 +1,8 @@
 <script>
   import { StatusBar, Style } from "@capacitor/status-bar"
+  import { onMount } from "svelte"
+
+  onMount(setStatusBar)
 
   async function setStatusBar() {
     await StatusBar.setStyle({ style: Style.Dark })
