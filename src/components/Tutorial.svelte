38b3536566cc98d85309618add9082b4d8e050d2
Mitchel <mitchel_jager@hotmail.com>
2022-09-19 11:46:42 +0200

    Improve tutorial

--- src/components/Tutorial.svelte
+++ src/components/Tutorial.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { fade, scale } from "svelte/transition"
   import { screen } from "../stores/screen.js"
   import { cells, cellShapes } from "../stores/cells.js"
   import { enableSfx } from "../stores/settings.js"
@@ -6,6 +7,7 @@
 
   let level = -1
   let tutorialComplete = false
+  let text = ""
 
   $: setCellsForLevel(level)
   $: if ($cells.length == 0 && level < 4) level++
@@ -16,6 +18,8 @@
 
   function setCellsForLevel() {
     if (level == 0) {
+      text = "The objective is to <strong>connect pipes</strong> between two points <br><br> <span>Tap to rotate</span>"
+
       $cells = [
         { id: randomString(), x: 0, y: 0, connected_to: -1, shape: $cellShapes[3].shape },
         { id: randomString(), x: 1, y: -1, connected_to: -1, shape: $cellShapes[3].shape },
@@ -25,6 +29,8 @@
         { id: randomString(), x: 5, y: -5, connected_to: -1, shape: $cellShapes[3].shape }
       ]
     } else if (level == 1) {
+      text = "You can connect to any point <strong>regardless of color</strong> <br><br> <span>Tap to rotate</span>"
+
       $cells = [
         { id: randomString(), x: 0, y: 0, connected_to: -1, shape: $cellShapes[3].shape },
         { id: randomString(), x: 1, y: -1, connected_to: -1, shape: $cellShapes[3].shape },
@@ -32,9 +38,14 @@
         { id: randomString(), x: 3, y: -3, connected_to: -1, shape: $cellShapes[3].shape },
         { id: randomString(), x: 4, y: -4, connected_to: -1, shape: $cellShapes[3].shape },
         { id: randomString(), x: 5, y: -5, connected_to: -1, shape: [[0, 1, 0], [1, 1, 0], [0, 0, 0]] },
-        { id: randomString(), x: 5, y: -6, connected_to: -1, shape: [[0, 1, 0], [0, 1, 1], [0, 0, 0]] }
+        { id: randomString(), x: 5, y: -6, connected_to: -1, shape: [[0, 1, 0], [0, 1, 0], [0, 1, 0]] },
+        { id: randomString(), x: 5, y: -7, connected_to: -1, shape: [[0, 1, 0], [0, 1, 0], [0, 1, 0]] },
+        { id: randomString(), x: 5, y: -8, connected_to: -1, shape: [[0, 1, 0], [0, 1, 0], [0, 1, 0]] },
+        { id: randomString(), x: 5, y: -9, connected_to: -1, shape: [[0, 1, 0], [0, 1, 1], [0, 0, 0]] }
       ]
     } else if (level == 2) {
+      text = "You can connect to any <strong>side</strong> <br><br> <span>Tap to rotate</span>"
+
       $cells = [
         { id: randomString(), x: 0, y: 0, connected_to: -1, shape: $cellShapes[3].shape },
         { id: randomString(), x: 0, y: -1, connected_to: -1, shape: $cellShapes[3].shape },
@@ -42,6 +53,8 @@
         { id: randomString(), x: 1, y: -3, connected_to: -1, shape: [[0, 1, 0], [1, 1, 0], [0, 0, 0]] },
       ]
     } else if (level == 3) {
+      text = "Cells can be <strong>moved</strong> by dragging <br><br> <span>Tap and drag to move</span>"
+
       $cells = [
         { id: randomString(), x: 0, y: 0, connected_to: -1, shape: $cellShapes[3].shape },
         { id: randomString(), x: 1, y: -1, connected_to: -1, shape: $cellShapes[3].shape },
@@ -51,6 +64,7 @@
         { id: randomString(), x: 5, y: -4, connected_to: -1, shape: $cellShapes[3].shape }
       ]
     } else if (level == 4) {
+      text = ""
       tutorialComplete = true
     }
 
@@ -66,8 +80,8 @@
 </script>
 
 { #if tutorialComplete }
-  <div class="tutorial-complete">
-    <div class="tutorial-complete__title">
+  <div class="tutorial-complete" in:fade={{ duration: 500, delay: 500 }}>
+    <div class="tutorial-complete__title" in:scale={{ duration: 500, delay: 750 }}>
       <small>[</small>Complete<small>]</small>
     </div>
 
@@ -78,6 +92,12 @@
 <main class="board">
 	<div class="board__header">
     <div class="button" on:click={ () => { $screen = "menu" } }><span>&gt;</span> [Menu]</div>
+
+    <div class="progress">
+      { level + 1 }/4
+    </div>
+
+    <div class="text">{@html text}</div>
   </div>
 
 	<div class="board__content">
@@ -119,7 +139,7 @@
   .button {
     border: 0;
     color: #c08706;
-    font-size: 28px;
+    font-size: clamp(24px, 8vw, 28px);
     font-weight: bold;
     cursor: pointer;
 
@@ -128,25 +148,14 @@
     }
   }
 
-  @keyframes fade-in-tutorial-complete {
-    from {
-      opacity: 0;
-    }
-
-    to {
-      opacity: 1;
-    }
-  }
-
   .tutorial-complete {
-    text-align: center;
     position: fixed;
     left: 50%;
     top: 50%;
+    width: 100%;
     transform: translateX(-50%) translateY(-50%);
-    opacity: 0;
     text-transform: lowercase;
-    animation: fade-in-tutorial-complete 250ms 500ms forwards;
+    text-align: center;
   }
 
   .tutorial-complete__title {
@@ -158,6 +167,29 @@
     small {
       font-size: 2rem;
       color: #738b98;
+    }
+  }
+
+  .progress {
+    margin-left: auto;
+  }
+
+  .text {
+    position: fixed;
+    width: 100%;
+    padding: 2rem 1rem;
+    top: 7.15rem;
+    left: 0;
+    background: #080b0d77;
+    font-size: clamp(24px, 5vw, 28px);
+    text-align: center;
+
+    :global(strong) {
+      color: #c08706;
+    }
+
+    :global(span) {
+      color: #bd1d46;
     }
   }
 </style>
